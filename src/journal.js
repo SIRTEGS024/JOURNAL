c6bc48a3c346a3be90a8ab6268e2914e335da218
tega <kesienametitiri024@gmail.com>
2022-05-12 11:35:11 +0100

    add src files

---
+++ src/journal.js
@@ -0,0 +1,27 @@
+export default function Journal(title, body) {
+  this.title = title;
+  this.body = body;
+}
+
+Journal.prototype.wordCounter = function () {
+  let totalWords = 0;
+  let wordArray = this.body.split(" ");
+  totalWords = wordArray.length;
+  return totalWords;
+};
+
+Journal.prototype.charCounter = function () {
+  let vowelArr = ["a", "i", "o", "e", "u"];
+  let totalVowels = 0;
+  let totalConsonants = 0;
+
+  for (let i = 0; i < this.body.length; i++) {
+    if (vowelArr.includes(this.body[i])) {
+      totalVowels++;
+    }
+    else if (this.body[i] != " ") {
+      totalConsonants++;
+    }
+  }
+  return [totalVowels, totalConsonants];
+};
